9432e5979218d364d1cef06137c8c9173cc35c8d
https://github.com/ <https://github.com/>
2021-07-29 13:40:54 -0300

    projeto inicial

---
+++ src/main/java/org/rede_social/knowledgeDomainKD/KnowledgeDomainKdApplication.java
@@ -0,0 +1,13 @@
+package org.rede_social.knowledgeDomainKD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeDomainKdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeDomainKdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rede_social/knowledgeDomainKD/KnowledgeDomainKdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rede_social.knowledgeDomainKD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeDomainKdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
