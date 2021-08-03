85430adc5fa398215702e561d7002c527697ef55
unknown <rhogermiranda@gmail.com>
2021-08-03 11:50:13 -0300

    Criado o metodo getById

--- src/main/java/org/rede_social/knowledgeDomainKD/controller/TemaController.java
+++ src/main/java/org/rede_social/knowledgeDomainKD/controller/TemaController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +32,13 @@ public class TemaController {
 	@GetMapping(value = "/getAll")
 	public ResponseEntity<List<Tema>> getAll(){
 		return ResponseEntity.status(HttpStatus.OK).body(temaRepository.findAll());
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Tema> getById(@PathVariable Long id) {
+		return temaRepository.findById(id).map(resp -> ResponseEntity.ok(resp))
+                .orElse(ResponseEntity.notFound().build());
+		
 	}
 	
 	
