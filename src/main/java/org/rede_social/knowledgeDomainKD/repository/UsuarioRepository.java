4b6f2ec3c8e53394750cfdd9f2cc5ad760477f38
João Junior <joaodcpjunior@gmail.com>
2021-08-10 10:36:34 -0300

    Criado configuração UserDetails

--- src/main/java/org/rede_social/knowledgeDomainKD/repository/UsuarioRepository.java
+++ src/main/java/org/rede_social/knowledgeDomainKD/repository/UsuarioRepository.java
@@ -7,9 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 	Optional<Usuario> findByEmail(String email);
-	
 
 }
--- src/main/java/org/rede_social/knowledgeDomainKD/security/UserDetailsImplements.java
+++ src/main/java/org/rede_social/knowledgeDomainKD/security/UserDetailsImplements.java
@@ -1,5 +1,66 @@
 package org.rede_social.knowledgeDomainKD.security;
 
-public class UserDetailsImplements {
+import java.util.Collection;
+import java.util.List;
+
+import org.rede_social.knowledgeDomainKD.model.Usuario;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImplements implements UserDetails {
+	private static final long serialVersionUID = 1L;
+	
+	private String email;
+	private String password;
+	private List<GrantedAuthority> authorities;
+	
+	public UserDetailsImplements() {	}
+	
+	public UserDetailsImplements(Usuario usuario) {
+		this.email = usuario.getEmail();
+		this.password = usuario.getSenha();
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
