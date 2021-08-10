e01b0d99b995f3f65ec962d8bec21ecce2354839
Thiago mendes <thiagomendes0601@gmail.com>
2021-08-10 09:53:54 -0300

    Implementado o Crud do usuario

---
+++ src/main/java/org/rede_social/knowledgeDomainKD/controller/UsuarioController.java
@@ -0,0 +1,42 @@
+package org.rede_social.knowledgeDomainKD.controller;
+
+import java.util.List;
+
+import org.rede_social.knowledgeDomainKD.model.Usuario;
+import org.rede_social.knowledgeDomainKD.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/usuario")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepository repository;
+	
+	@GetMapping("/getAll")
+	public ResponseEntity<List<Usuario>> getAll(){
+		List<Usuario> listaDeUsuarios = repository.findAll();
+		if (!listaDeUsuarios.isEmpty()) {
+			return ResponseEntity.status(200).body(listaDeUsuarios);
+		} else {
+			return ResponseEntity.status(204).build();
+		}
+	}
+	
+	@GetMapping("/id/{idUsuario}")
+	public ResponseEntity<Usuario> getById(@PathVariable Long idUsuario){
+		return repository.findById(idUsuario)
+				.map(usuarioExistente -> ResponseEntity.status(200).body(usuarioExistente))
+				.orElse(ResponseEntity.status(204).build());
+	}
+	
+	
+
+}
