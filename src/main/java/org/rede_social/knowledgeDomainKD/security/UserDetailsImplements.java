25dd3a3d64a618fd1e7c4bb4cfea3f432653d4b4
João Junior <joaodcpjunior@gmail.com>
2021-08-12 13:35:34 -0300

    Corrigido configuração de segurança

--- src/main/java/org/rede_social/knowledgeDomainKD/security/UserDetailsImplements.java
+++ src/main/java/org/rede_social/knowledgeDomainKD/security/UserDetailsImplements.java
@@ -9,58 +9,59 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 public class UserDetailsImplements implements UserDetails {
 	private static final long serialVersionUID = 1L;
-	
+
 	private String email;
-	private String password;
+	private String senha;
 	private List<GrantedAuthority> authorities;
-	
-	public UserDetailsImplements() {	}
-	
+
+	public UserDetailsImplements() {
+	}
+
 	public UserDetailsImplements(Usuario usuario) {
 		this.email = usuario.getEmail();
-		this.password = usuario.getSenha();
+		this.senha = usuario.getSenha();
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return senha;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
 	}
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	@Override
-	public String getPassword() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
 }
